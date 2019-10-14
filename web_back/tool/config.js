8df140a4623beea624390679ea87035854a6779c
itxcr <52365573@qq.com>
2019-10-15 02:46:15 +0800

    登录功能实现

--- web_back/tool/config.js
+++ web_back/tool/config.js
@@ -1,8 +1,8 @@
-// config - 中文含义为配置
-// - 我们可以将项目中使用的接口地址统一保存在这个文件中
+// 将项目中使用的接口地址统一保存在这个文件中
+// 将基地址保存为一个变量然后进行拼接，方便后期维护
 
-var baseUrl = 'http://localhost:8000';
+var BASEURL = 'http://localhost:8000/';
 
-var USER_LOGIN = baseUrl + '/admin/login';
-var USER_LOGOUT = baseUrl + '/admin/logout';
-var USER_INFO = baseUrl + '/admin/login';
+var USER_LOGIN = BASEURL + 'admin/login';
+var USER_LOGOUT = BASEURL + 'admin/logout';
+var USER_INFO = BASEURL + 'admin/getuser';
