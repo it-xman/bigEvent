4144345f2f03f891d37bce97859caff64c201a27
itxcr <52365573@qq.com>
2019-10-15 13:58:17 +0800

    格式化代码，将用户登陆接口写到user.js里进行管理

---
+++ web_back/tool/user.js
@@ -0,0 +1,18 @@
+//这个user.js用来管理用户进行的所有接口请求操作
+var user = {
+  // login方法用来管理用户登录接口的操作方式
+  // - 将页面中的功能提取到user.js后需要区分哪些代码和接口有关，哪些代码和页面有关
+  // - 和接口有关的代码留在user.js，和页面有关的部分放回页面，通过传参方式传入
+  // 这里的options是一个对象形式的参数
+  login: function (options) {
+    $.ajax({
+      type: 'post',
+      url: USER_LOGIN,
+      data: options.data,
+      success: options.callback,
+    })
+  },
+  logout: function () {
+
+  }
+};
